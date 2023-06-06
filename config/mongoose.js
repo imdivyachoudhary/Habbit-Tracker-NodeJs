e9b4421b79e518722853594f55f10cbf3987d1ee
Divya Choudhary <imdivyachoudhary@gmail.com>
2023-06-06 15:48:49 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 async function main() {
   // await mongoose.connect(`mongodb+srv://codeialtest1234:aman1234@cluster0.jnofoef.mongodb.net/?retryWrites=true&w=majority`);
-  // await mongoose.connect(`mongodb+srv://imdivyachoudhary:divya123@cluster0.lg0g4rt.mongodb.net/habbit_tracker?retryWrites=true&w=majority`);
-  await mongoose.connect(`mongodb+srv://tested:test1234@cluster0.3gzaffk.mongodb.net/`);
+  await mongoose.connect(`mongodb+srv://imdivyachoudhary:divya123@cluster0.lg0g4rt.mongodb.net/habbit_tracker?retryWrites=true&w=majority`);
+  // await mongoose.connect(`mongodb+srv://tested:test1234@cluster0.3gzaffk.mongodb.net/`);
   console.log(`Connected to DB : habbit_tracker`);
 }
 
