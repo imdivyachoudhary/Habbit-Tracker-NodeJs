548c6dfe01aefc6b4882166b6a8c1a8f643af7c4
imdivyachoudhary <divyachoudhary.avnc@gmail.com>
2023-07-24 13:47:37 +0530

    comment update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+app.use(express.json()); //to parse the json body received in api request
 
 const cors = require("cors");
 app.use(cors());
